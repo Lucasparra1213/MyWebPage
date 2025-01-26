6bde50e1636a8662073f2346c2d6fdd4fcff565d
Lucas Parra <lucasparra1213@gmail.com>
2025-01-26 15:16:25 -0500

    about logos added

--- src/assets/components/About.jsx
+++ src/assets/components/About.jsx
@@ -8,7 +8,34 @@ const About = () => {
         <p className='text'>Hi! My name is Lucas. I am a fresh bootcamp graduate with a bachelor's degree in Management Information Systems looking to jumpstart his career as a UI developer. 
         I strongly appreciate a good UI can uplift a company's brand and create a stronger engagement with it's users.</p>
         <p>During my studies, I gained experience in React and CSS as the main tools for front-end development, but I am always looking to learn new technologies that can enhance my skill repertoire</p>
+            <h1>MY SKILLS</h1>
+        <div className='skills-container'>
+            <div className='javaScript'>
+                <img src='/Images/JavaScript logo.png'></img>
+                <h2>JavaScript</h2>
+            </div>
+            <div className='react'>
+                <img src='/Images/React logo.png'></img>
+                <h2>React</h2>
+            </div>
+            <div className='css'>
+                <img src='/Images/CSS.png'></img>
+                <h2>CSS</h2>
+            </div>
+            <div className='html'>
+                <img src='/Images/HTML logo.png'></img>
+                <h2>HTML</h2>
+            </div>
+            <div className='mongoDB'>
+                <img src='/Images/MongoDB logo.png'></img>
+                <h2>MongoDB</h2>
+            </div>
+            <div className='node'>
+                <img src='/Images/NodeJs logo.png'></img>
+                <h2>NodeJS</h2>
+            </div>
 
+        </div>
     </div>
 )
 }
